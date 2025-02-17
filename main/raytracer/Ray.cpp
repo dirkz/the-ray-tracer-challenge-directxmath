57a38be22939c7924e2630a3a1544b720764efde
Dirk Zimmermann <me@dirkz.com>
2025-02-17 16:29:05 +0100

    Accessing ray members

--- main/raytracer/Ray.cpp
+++ main/raytracer/Ray.cpp
@@ -3,4 +3,10 @@
 namespace zrt
 {
 
+Ray::Ray(FXMVECTOR origin, FXMVECTOR direction)
+{
+    XMStoreFloat4(&m_origin, origin);
+    XMStoreFloat4(&m_direction, direction);
 }
+
+} // namespace zrt
--- main/raytracer/Ray.h
+++ main/raytracer/Ray.h
@@ -7,6 +7,21 @@ namespace zrt
 
 struct Ray
 {
+    Ray(FXMVECTOR origin, FXMVECTOR direction);
+
+    inline XMVECTOR Origin() const
+    {
+        return XMLoadFloat4(&m_origin);
+    }
+
+    inline XMVECTOR Direction() const
+    {
+        return XMLoadFloat4(&m_direction);
+    }
+
+  private:
+    XMFLOAT4 m_origin;
+    XMFLOAT4 m_direction;
 };
 
 } // namespace zrt
