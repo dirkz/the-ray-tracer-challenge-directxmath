76a457ac111bda3f6c4c3f558599a7a54fb88769
Dirk Zimmermann <me@dirkz.com>
2025-02-17 18:50:50 +0100

    Complete sphere intersection (wrong)

--- main/raytracer/Sphere.cpp
+++ main/raytracer/Sphere.cpp
@@ -8,11 +8,44 @@ std::vector<float> Sphere::Intersect(const Ray &ray)
     // The sphere is at the origin
     XMVECTOR sphereToRay = ray.Origin();
 
-    XMVECTOR rdDot = XMVector4Dot(ray.Direction(), ray.Direction());
-    XMVECTOR b = XMVectorScale(XMVector4Dot(ray.Direction(), sphereToRay), 2);
-    XMVECTOR c = XMVectorSubtract(XMVector4Dot(sphereToRay, sphereToRay), XMVectorReplicate(1));
+    XMVECTOR va = XMVector4Dot(ray.Direction(), ray.Direction());
+    XMVECTOR vb = XMVectorScale(XMVector4Dot(ray.Direction(), sphereToRay), 2);
+    XMVECTOR vc = XMVectorSubtract(XMVector4Dot(sphereToRay, sphereToRay), XMVectorReplicate(1));
 
-    return std::vector<float>();
+    XMVECTOR vDiscriminant =
+        XMVectorSubtract(XMVectorMultiply(vb, vb), XMVectorScale(XMVectorMultiply(va, vc), 2));
+
+    XMFLOAT4 floatsD;
+    XMStoreFloat4(&floatsD, vDiscriminant);
+
+    float discriminant = floatsD.x;
+
+    if (discriminant < 0)
+    {
+        return {};
+    }
+    else
+    {
+        XMFLOAT4 floatsA, floatsB;
+        XMStoreFloat4(&floatsA, va);
+        XMStoreFloat4(&floatsB, vb);
+
+        float a = floatsA.x;
+        float b = floatsB.x;
+
+        float rootD = sqrt(discriminant);
+        float t1 = (-b - rootD) / (2 * a);
+        float t2 = (-b + rootD) / (2 * a);
+
+        if (t1 < t2)
+        {
+            return {t1, t2};
+        }
+        else
+        {
+            return {t2, t2};
+        }
+    }
 }
 
 } // namespace zrt
