cbc54b682c5eca2ea0c0980fb21dd9b37a7f441e
Dirk Zimmermann <me@dirkz.com>
2025-02-17 19:49:45 +0100

    Intersection

---
+++ main/raytracer/Intersection.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include "Intersectable.h"
+
+namespace zrt
+{
+
+struct Intersection
+{
+    inline Intersection(const Intersectable *pIntersectable, float t)
+        : m_intersectable{pIntersectable}, m_t{t} {};
+
+    const Intersectable *GetIntersectable() const
+    {
+        return m_intersectable;
+    }
+
+    inline float GetT() const
+    {
+        return m_t;
+    }
+
+  private:
+    const Intersectable *m_intersectable;
+    float m_t;
+};
+
+} // namespace zrt
