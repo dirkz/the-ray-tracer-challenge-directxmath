e0afd65f883f43357e668a20409bab821d41b99d
Dirk Zimmermann <me@dirkz.com>
2025-02-17 18:04:33 +0100

    Use initializer lists

--- main/tests/RayTest.cpp
+++ main/tests/RayTest.cpp
@@ -12,14 +12,14 @@ TEST(RayTest, CreatingAndQueryingRays)
     ASSERT_TRUE(IsPoint(origin));
     auto direction = Vector(4, 5, 6);
     ASSERT_TRUE(IsVector(direction));
-    auto ray = Ray(origin, direction);
+    auto ray = Ray{origin, direction};
     ASSERT_TRUE(IsEqual(origin, ray.Origin()));
     ASSERT_TRUE(IsEqual(direction, ray.Direction()));
 }
 
 TEST(RayTest, ComputingPointFromDistance)
 {
-    auto ray = Ray(Point(2, 3, 4), Vector(1, 0, 0));
+    auto ray = Ray{Point(2, 3, 4), Vector(1, 0, 0)};
     ASSERT_TRUE(IsEqual(ray.Position(0), Point(2, 3, 4)));
     ASSERT_TRUE(IsEqual(ray.Position(1), Point(3, 3, 4)));
     ASSERT_TRUE(IsEqual(ray.Position(-1), Point(1, 3, 4)));
