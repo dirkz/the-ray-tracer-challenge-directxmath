060e729e58cc96fc6bf2856834bd07ad34ee8154
Dirk Zimmermann <me@dirkz.com>
2025-02-18 10:13:02 +0100

    Start intersection test

--- main/tests/IntersectionTest.cpp
+++ main/tests/IntersectionTest.cpp
@@ -1,13 +1,18 @@
 #include <gtest/gtest.h>
 
-#include "Ray.h"
-#include "Vector.h"
+#include "Sphere.h"
+#include "Intersection.h"
 
 namespace zrt
 {
 
 TEST(IntersectionTest, HitWhenAllIntersectionsHavePositiveT)
 {
+    Sphere s{};
+    Intersection i1{s, 1};
+    Intersection i2{s, 2};
+    IntersectionList xs = instersections({i1, i2});
+    const Intersection *pI = hit(xs);
 }
 
 } // namespace zrt
