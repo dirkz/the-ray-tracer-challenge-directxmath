3e72d4e5947a9c748b840d895f4757cde71971d3
Dirk Zimmermann <me@dirkz.com>
2025-02-18 20:29:54 +0100

    Test reflecting vectors

--- main/tests/VectorTest.cpp
+++ main/tests/VectorTest.cpp
@@ -1,12 +1,25 @@
 #include <gtest/gtest.h>
 
+#include "TestConstants.h"
 #include "Vector.h"
 
 namespace zrt
 {
 
+TEST(VectorTest, ReflectingVectorApproaching45)
+{
+    auto v = Vector(1, -1, 0);
+    auto n = Vector(0, 1, 0);
+    auto r = Reflect(v, n);
+    EXPECT_EQ(Floats(r), Floats(Vector(1, 1, 0)));
+}
+
 TEST(VectorTest, ReflectingVectorOffSlantedSurface)
 {
+    auto v = Vector(0, -1, 0);
+    auto n = Vector(HalfSqrt, HalfSqrt, 0);
+    auto r = Reflect(v, n);
+    EXPECT_EQ(Floats(r), Floats(Vector(1, 0, 0)));
 }
 
 } // namespace zrt
