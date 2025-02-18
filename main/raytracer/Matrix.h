1de5fcbf8bcd3bc051f506fc5aead1c5cade249b
Dirk Zimmermann <me@dirkz.com>
2025-02-18 16:10:09 +0100

    Rotation about X (test fails because of rounding)

--- main/raytracer/Matrix.h
+++ main/raytracer/Matrix.h
@@ -15,4 +15,19 @@ inline XMMATRIX Scaling(float x, float y, float z)
     return XMMatrixScaling(x, y, z);
 }
 
+inline XMMATRIX RotationX(float angle)
+{
+    return XMMatrixRotationX(angle);
+}
+
+inline XMMATRIX RotationY(float angle)
+{
+    return XMMatrixRotationY(angle);
+}
+
+inline XMMATRIX RotationZ(float angle)
+{
+    return XMMatrixRotationZ(angle);
+}
+
 } // namespace zrt
---
+++ main/tests/TransformationTest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include "Vector.h"
+#include "Matrix.h"
+
+namespace zrt
+{
+
+TEST(TransformationTest, RotatingPointAroundX)
+{
+    auto pi = std::numbers::pi_v<float>;
+    auto halfSqrt = sqrt(2.f) / 2;
+    auto p = Point(0, 1, 0);
+    auto halfQuarter = RotationX(pi / 4);
+    auto fullQuarter = RotationX(pi / 2);
+    ASSERT_EQ(Floats(XMVector4Transform(p, halfQuarter)), Floats(Point(0, halfSqrt, halfSqrt)));
+    ASSERT_EQ(Floats(XMVector4Transform(p, fullQuarter)), Floats(Point(0, 0, 1)));
+}
+
+} // namespace zrt
