84e23e3f06622f815ad76dc53df27f20abd85c41
Dirk Zimmermann <me@dirkz.com>
2025-02-18 10:58:19 +0100

    Complete intersection tests (one failing)

--- main/tests/IntersectionTest.cpp
+++ main/tests/IntersectionTest.cpp
@@ -13,6 +13,42 @@ TEST(IntersectionTest, HitWhenAllIntersectionsHavePositiveT)
     Intersection i2{s, 2};
     IntersectionList xs = instersections({i1, i2});
     const Intersection *pI = hit(xs);
+    ASSERT_NE(pI, nullptr);
+    ASSERT_EQ(*pI, i1);
+}
+
+TEST(IntersectionTest, HitWhenSomeIntersectionsHaveNegativeT)
+{
+    Sphere s{};
+    Intersection i1{s, -1};
+    Intersection i2{s, 1};
+    IntersectionList xs = instersections({i1, i2});
+    const Intersection *pI = hit(xs);
+    ASSERT_NE(pI, nullptr);
+    ASSERT_EQ(*pI, i2);
+}
+
+TEST(IntersectionTest, HitWhenAllIntersectionsHaveNegativeT)
+{
+    Sphere s{};
+    Intersection i1{s, -2};
+    Intersection i2{s, -1};
+    IntersectionList xs = instersections({i1, i2});
+    const Intersection *pI = hit(xs);
+    ASSERT_EQ(pI, nullptr);
+}
+
+TEST(IntersectionTest, HitIsAlwaysLowestNonnegativeIntersection)
+{
+    Sphere s{};
+    Intersection i1{s, 5};
+    Intersection i2{s, 7};
+    Intersection i3{s, -3};
+    Intersection i4{s, 2};
+    IntersectionList xs = instersections({i1, i2, i3, i4});
+    const Intersection *pI = hit(xs);
+    ASSERT_NE(pI, nullptr);
+    ASSERT_EQ(*pI, i4);
 }
 
 } // namespace zrt
