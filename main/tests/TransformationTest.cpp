215a0e45eda139a0d0d7e5cf5537b81bcc43e8ca
Dirk Zimmermann <me@dirkz.com>
2025-02-18 16:30:52 +0100

    Test rotation around y

--- main/tests/TransformationTest.cpp
+++ main/tests/TransformationTest.cpp
@@ -12,8 +12,8 @@ const float halfSqrt = sqrt(2.f) / 2;
 TEST(TransformationTest, RotatingPointAroundX)
 {
     auto p = Point(0, 1, 0);
-    auto halfQuarter = RotationX(pi / 4);
-    auto fullQuarter = RotationX(pi / 2);
+    const XMMATRIX halfQuarter = RotationX(pi / 4);
+    const XMMATRIX fullQuarter = RotationX(pi / 2);
     EXPECT_EQ(Floats(XMVector4Transform(p, halfQuarter)), Floats(Point(0, halfSqrt, halfSqrt)));
     EXPECT_EQ(Floats(XMVector4Transform(p, fullQuarter)), Floats(Point(0, 0, 1)));
 }
@@ -21,9 +21,18 @@ TEST(TransformationTest, RotatingPointAroundX)
 TEST(TransformationTest, InverseOfXRotationRotatesInOppositeDirection)
 {
     auto p = Point(0, 1, 0);
-    auto halfQuarter = RotationX(pi / 4);
+    const XMMATRIX halfQuarter = RotationX(pi / 4);
     auto inv = XMMatrixInverse(nullptr, halfQuarter);
     EXPECT_EQ(Floats(XMVector4Transform(p, inv)), Floats(Point(0, halfSqrt, -halfSqrt)));
+}
+
+TEST(TransformationTest, RotatingPointAroundY)
+{
+    auto p = Point(0, 0, 1);
+    const XMMATRIX halfQuarter = RotationY(pi / 4);
+    const XMMATRIX fullQuarter = RotationY(pi / 2);
+    EXPECT_EQ(Floats(XMVector4Transform(p, halfQuarter)), Floats(Point(halfSqrt, 0, halfSqrt)));
+    EXPECT_EQ(Floats(XMVector4Transform(p, fullQuarter)), Floats(Point(1, 0, 0)));
 }
 
 } // namespace zrt
