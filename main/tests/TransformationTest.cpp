9abe241a5bd826fc56ffff16f8e7f44390ee8c26
Dirk Zimmermann <me@dirkz.com>
2025-02-19 20:21:00 +0100

    View transform tests

--- main/tests/TransformationTest.cpp
+++ main/tests/TransformationTest.cpp
@@ -43,4 +43,42 @@ TEST(TransformationTest, RotatingPointAroundZ)
     EXPECT_EQ(Floats(XMVector4Transform(p, fullQuarter)), Floats(Point(-1, 0, 0)));
 }
 
+TEST(TransformationTest, TransformationMatrixDefaultOrientation)
+{
+    auto from = Point(0, 0, 0);
+    auto to = Point(0, 0, -1);
+    auto up = Vector(0, 1, 0);
+    auto t = ViewTransform(from, to, up);
+    EXPECT_EQ(Floats(t), Floats(XMMatrixIdentity()));
+}
+
+TEST(TransformationTest, TransformationMatrixLookingPositiveZ)
+{
+    auto from = Point(0, 0, 0);
+    auto to = Point(0, 0, 1);
+    auto up = Vector(0, 1, 0);
+    auto t = ViewTransform(from, to, up);
+    EXPECT_EQ(Floats(t), Floats(Scaling(-1, 1, -1)));
+}
+
+TEST(TransformationTest, TransformationMatrixMovesWorld)
+{
+    auto from = Point(0, 0, 8);
+    auto to = Point(0, 0, 0);
+    auto up = Vector(0, 1, 0);
+    auto t = ViewTransform(from, to, up);
+    EXPECT_EQ(Floats(t), Floats(Translation(0, 0, -8)));
+}
+
+TEST(TransformationTest, TransformationMatrixArbitrary)
+{
+    auto from = Point(1, 3, 2);
+    auto to = Point(4, -2, 8);
+    auto up = Vector(0, 1, 0);
+    auto t = ViewTransform(from, to, up);
+    auto m = XMMATRIX(-0.50709f, 0.50709f, 0.67612f, -2.36643, 0.76772f, 0.60609f, 0.12122f,
+                      -2.82843f, -0.35857f, 0.59761f, -0.71714f, 0.f, 0.f, 0.f, 0.f, 1.f);
+    EXPECT_EQ(Floats(t), Floats(m));
+}
+
 } // namespace zrt
