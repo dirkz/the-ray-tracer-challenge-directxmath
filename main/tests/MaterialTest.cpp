752542be3a7a004b2f715cc5e4805fb28c2e7942
Dirk Zimmermann <me@dirkz.com>
2025-02-19 10:36:14 +0100

    A whole bunch of lighting tests

--- main/tests/MaterialTest.cpp
+++ main/tests/MaterialTest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "Material.h"
+#include "TestConstants.h"
 #include "TestUtil.h"
 #include "Vector.h"
 
@@ -15,6 +16,61 @@ TEST(MaterialTest, DefaultMaterial)
     EXPECT_FLOAT_EQ(m.Diffuse(), 0.9f);
     EXPECT_FLOAT_EQ(m.Specular(), 0.9f);
     EXPECT_FLOAT_EQ(m.Shininess(), 200.f);
+}
+
+TEST(MaterialTest, LightingWithEyeBetweenLightAndSurface)
+{
+    Material m{};
+    auto position = Point(0, 0, 0);
+    auto eyev = Vector(0, 0, -1);
+    auto normalv = Vector(0, 0, -1);
+    auto light = PointLight{Point(0, 0, -10), Color(1, 1, 1)};
+    auto result = m.Lighting(light, position, eyev, normalv);
+    EXPECT_EQ(Floats(result), Floats(Color(1.9f, 1.9f, 1.9f)));
+}
+
+TEST(MaterialTest, LightingWithEyeBetweenLightAndSurfaceEyeOffset45)
+{
+    Material m{};
+    auto position = Point(0, 0, 0);
+    auto eyev = Vector(0, HalfSqrt, -HalfSqrt);
+    auto normalv = Vector(0, 0, -1);
+    auto light = PointLight{Point(0, 0, -10), Color(1, 1, 1)};
+    auto result = m.Lighting(light, position, eyev, normalv);
+    EXPECT_EQ(Floats(result), Floats(Color(1.0f, 1.0f, 1.0f)));
+}
+
+TEST(MaterialTest, LightingWithEyeOppositeSurfaceEyeOffset45)
+{
+    Material m{};
+    auto position = Point(0, 0, 0);
+    auto eyev = Vector(0, 0, -1);
+    auto normalv = Vector(0, 0, -1);
+    auto light = PointLight{Point(0, 10, -10), Color(1, 1, 1)};
+    auto result = m.Lighting(light, position, eyev, normalv);
+    EXPECT_EQ(Floats(result), Floats(Color(0.7364f, 0.7364f, 0.7364f)));
+}
+
+TEST(MaterialTest, LightingWithEyeInPathOfReflectionVector)
+{
+    Material m{};
+    auto position = Point(0, 0, 0);
+    auto eyev = Vector(0, -HalfSqrt, -HalfSqrt);
+    auto normalv = Vector(0, 0, -1);
+    auto light = PointLight{Point(0, 10, -10), Color(1, 1, 1)};
+    auto result = m.Lighting(light, position, eyev, normalv);
+    EXPECT_EQ(Floats(result), Floats(Color(1.6364f, 1.6364f, 1.6364f)));
+}
+
+TEST(MaterialTest, LightingWithLightBehindSurface)
+{
+    Material m{};
+    auto position = Point(0, 0, 0);
+    auto eyev = Vector(0, 0, -1);
+    auto normalv = Vector(0, 0, -1);
+    auto light = PointLight{Point(0, 0, 10), Color(1, 1, 1)};
+    auto result = m.Lighting(light, position, eyev, normalv);
+    EXPECT_EQ(Floats(result), Floats(Color(0.1f, 0.1f, 0.1f)));
 }
 
 } // namespace zrt
