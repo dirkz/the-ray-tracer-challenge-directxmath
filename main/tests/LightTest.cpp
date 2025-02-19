b5dc2c614d41842d61881b360fb3087077d27626
Dirk Zimmermann <me@dirkz.com>
2025-02-19 08:29:27 +0100

    Basic point light test

---
+++ main/tests/LightTest.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include "PointLight.h"
+#include "Vector.h"
+
+namespace zrt
+{
+
+TEST(LightTest, PointLightHasPositionAndIntensity)
+{
+    auto intensity = Color(1, 1, 1);
+    auto position = Point(0, 0, 0);
+    auto light = PointLight{position, intensity};
+    EXPECT_EQ(Floats(light.Position()), Floats(position));
+    EXPECT_EQ(Floats(light.Intensity()), Floats(intensity));
+}
+
+} // namespace zrt
