a2444aa3486eb01126d0ca443fdf22ca722d636d
Dirk Zimmermann <me@dirkz.com>
2025-02-19 20:32:23 +0100

    Implement ViewTransform

--- main/raytracer/Matrix.cpp
+++ main/raytracer/Matrix.cpp
@@ -5,7 +5,22 @@ namespace zrt
 
 XMMATRIX XM_CALLCONV ViewTransform(FXMVECTOR from, FXMVECTOR to, FXMVECTOR up)
 {
-    return XMMatrixIdentity();
+    XMVECTOR forward = XMVector4Normalize(XMVectorSubtract(to, from));
+    XMVECTOR backward = XMVectorScale(forward, -1);
+    XMVECTOR upn = XMVector4Normalize(up);
+    XMVECTOR left = XMVector3Cross(forward, upn);
+    XMVECTOR trueUp = XMVector3Cross(left, forward);
+
+    XMVECTOR lastRow = XMVECTOR{0, 0, 0, 1};
+    XMMATRIX orientation = XMMATRIX{left, trueUp, backward, lastRow};
+
+    XMFLOAT4 fromF;
+    XMStoreFloat4(&fromF, XMVectorScale(from, -1));
+    XMMATRIX translation = Translation(fromF.x, fromF.y, fromF.z);
+
+    XMMATRIX transform = XMMatrixMultiply(translation, orientation);
+
+    return transform;
 }
 
 } // namespace zrt
