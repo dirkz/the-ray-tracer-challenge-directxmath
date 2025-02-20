4022b9349bef746460a34d179acdcb1e676024d1
Dirk Zimmermann <me@dirkz.com>
2025-02-20 19:23:55 +0100

    Create a canvas

--- main/raytracer/Canvas.cpp
+++ main/raytracer/Canvas.cpp
@@ -1,6 +1,20 @@
 #include "Canvas.h"
 
+#include "Vector.h"
+
 namespace zrt
 {
 
+Canvas::Canvas(unsigned width, unsigned height)
+    : m_width{width}, m_height{height}, m_pixels(width * height)
+{
+    for (unsigned x = 0; x < width; ++x)
+    {
+        for (unsigned y = 0; y < height; ++y)
+        {
+            XMStoreFloat4(&m_pixels[y * width + x], Color(0, 0, 0));
+        }
+    }
 }
+
+} // namespace zrt
--- main/raytracer/Canvas.h
+++ main/raytracer/Canvas.h
@@ -7,6 +7,12 @@ namespace zrt
 
 struct Canvas
 {
+    Canvas(unsigned width, unsigned height);
+
+  private:
+    unsigned m_width;
+    unsigned m_height;
+    std::vector<XMFLOAT4> m_pixels;
 };
 
 } // namespace zrt
