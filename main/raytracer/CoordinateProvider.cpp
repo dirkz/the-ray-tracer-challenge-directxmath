8b2959098a0d2f8b304aa653f463e099c6001160
Dirk Zimmermann <me@dirkz.com>
2025-02-22 12:29:01 +0100

    Official coordinate provider

---
+++ main/raytracer/CoordinateProvider.cpp
@@ -0,0 +1,39 @@
+#include "CoordinateProvider.h"
+
+namespace zrt
+{
+
+CoordinateProvider::CoordinateProvider(unsigned maxX, unsigned maxY)
+    : m_maxX{maxX}, m_maxY{maxY}, m_currentX{0}, m_currentY{0}
+{
+}
+
+std::optional<CoordinateProvider::Coordinate> CoordinateProvider::Next()
+{
+    std::lock_guard l{m_mutex};
+
+    bool haveStillY = m_currentY < m_maxY - 1;
+    bool haveStillX = m_currentX < m_maxX - 1;
+
+    if (haveStillX || haveStillY)
+    {
+        if (haveStillX)
+        {
+            m_currentX++;
+            return Coordinate{m_currentX, m_currentY};
+        }
+        if (haveStillY)
+        {
+            m_currentY++;
+            m_currentX = 0;
+            return Coordinate{m_currentX, m_currentY};
+        }
+    }
+
+    assert(m_currentX == m_maxX - 1);
+    assert(m_currentY == m_maxY - 1);
+
+    return std::optional<Coordinate>{};
+}
+
+} // namespace zrt
---
+++ main/raytracer/CoordinateProvider.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "stdafx.h"
+
+namespace zrt
+{
+
+/// <summary>
+/// Generates all 2d coordinates on a given plane and provides access to them
+/// in a thread-safe manner.
+/// </summary>
+struct CoordinateProvider
+{
+    struct Coordinate
+    {
+        Coordinate(unsigned x, unsigned y) : m_x{x}, m_y{y} {};
+
+        inline unsigned X() const
+        {
+            return m_x;
+        }
+
+        inline unsigned Y() const
+        {
+            return m_y;
+        }
+
+      private:
+        unsigned m_x;
+        unsigned m_y;
+    };
+
+    CoordinateProvider(unsigned maxX, unsigned maxY);
+
+    /// <summary>
+    /// Thread-safe access to the next available coordinate, or nothing, if all work has been done.
+    /// </summary>
+    /// <returns></returns>
+    std::optional<Coordinate> Next();
+
+  private:
+    unsigned m_maxX;
+    unsigned m_maxY;
+    std::mutex m_mutex;
+    unsigned m_currentX;
+    unsigned m_currentY;
+};
+
+} // namespace zrt
