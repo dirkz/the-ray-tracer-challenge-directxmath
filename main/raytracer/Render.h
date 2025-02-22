1da2e6a03ed401cc687f9c7ef703031e398adb31
Dirk Zimmermann <me@dirkz.com>
2025-02-22 13:57:38 +0100

    Threaded renderer that can fall back to sequential

--- main/raytracer/Render.h
+++ main/raytracer/Render.h
@@ -30,4 +30,42 @@ template <class T> void RenderSequentially(const Camera &camera, const World &wo
     }
 }
 
+template <class T> void Render(const Camera &camera, const World &world, T &setPixelFn)
+{
+    unsigned numThreads = std::thread::hardware_concurrency();
+    if (numThreads == 0)
+    {
+        RenderSequentially(camera, world, setPixelFn);
+    }
+    else
+    {
+        CoordinateProvider provider{camera.HSize(), camera.VSize()};
+
+        auto renderFn = [&provider, &camera, &world, &setPixelFn]() {
+            auto optCoords = provider.Next();
+            if (!optCoords.has_value())
+            {
+                return;
+            }
+            unsigned x = optCoords.value().X();
+            unsigned y = optCoords.value().Y();
+            Ray ray = camera.RayForPixel(x, y);
+            auto color = world.ColorAt(ray);
+            setPixelFn(x, y, color);
+        };
+
+        std::vector<std::thread> threads(numThreads);
+        for (unsigned i = 0; i < numThreads; ++i)
+        {
+            std::thread thread{renderFn};
+            threads[i] = std::move(thread);
+        }
+
+        for (std::thread& thread : threads)
+        {
+            thread.join();
+        }
+    }
+}
+
 } // namespace zrt
--- main/tests/RenderTest.cpp
+++ main/tests/RenderTest.cpp
@@ -77,4 +77,24 @@ TEST(RenderTest, RenderSequential)
     EXPECT_EQ(Floats(Color(0.38066f, 0.47583f, 0.2855f)), Floats(color));
 }
 
+TEST(RenderTest, RenderThreaded)
+{
+    constexpr unsigned rows = 11;
+    constexpr unsigned cols = 11;
+    Canvas canvas{cols, rows};
+    CanvasWriter writer{canvas};
+
+    auto world = DefaultWorld();
+    auto from = Point(0, 0, -5);
+    auto to = Point(0, 0, 0);
+    auto up = Vector(0, 1, 0);
+    auto transform = ViewTransform(from, to, up);
+    Camera camera{11, 11, HalfPI, transform};
+
+    Render(camera, world, writer);
+
+    auto color = canvas.GetPixel(5, 5);
+    EXPECT_EQ(Floats(Color(0.38066f, 0.47583f, 0.2855f)), Floats(color));
+}
+
 } // namespace zrt
