1e0bdeaa945ffbb323eaa0a7a54fb8355e98b7c1
Dirk Zimmermann <me@dirkz.com>
2025-02-22 13:37:58 +0100

    Implement a sequential renderer with a function object

--- main/raytracer/Render.h
+++ main/raytracer/Render.h
@@ -4,6 +4,7 @@
 
 #include "Camera.h"
 #include "Canvas.h"
+#include "CoordinateProvider.h"
 #include "World.h"
 
 namespace zrt
@@ -11,4 +12,22 @@ namespace zrt
 
 Canvas RenderParallel(const Camera &camera, const World &world);
 
+template <class T> void RenderSequentially(const Camera &camera, const World &world, T &f)
+{
+    CoordinateProvider provider{camera.HSize(), camera.VSize()};
+    while (true)
+    {
+        auto optCoords = provider.Next();
+        if (!optCoords.has_value())
+        {
+            break;
+        }
+        unsigned x = optCoords.value().X();
+        unsigned y = optCoords.value().Y();
+        Ray ray = camera.RayForPixel(x, y);
+        auto color = world.ColorAt(ray);
+        f(x, y, color);
+    }
 }
+
+} // namespace zrt
--- main/tests/RenderTest.cpp
+++ main/tests/RenderTest.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 
+#include "Canvas.h"
 #include "CoordinateProvider.h"
+#include "Render.h"
+#include "TestConstants.h"
+#include "TestUtil.h"
 
 namespace zrt
 {
@@ -33,11 +37,44 @@ TEST(RenderTest, TraverseAllCoordinates)
         if (optCoord.has_value())
         {
             std::cerr << optCoord.value().X() << "," << optCoord.value().Y() << "\n";
-			coordinates.insert(optCoord.value());
+            coordinates.insert(optCoord.value());
         }
     }
 
     EXPECT_FALSE(provider.Next().has_value());
+}
+
+struct CanvasWriter
+{
+    CanvasWriter(Canvas &canvas) : m_canvas{canvas} {};
+
+    void XM_CALLCONV operator()(unsigned x, unsigned y, FXMVECTOR color)
+    {
+        m_canvas.SetPixel(x, y, color);
+    }
+
+  private:
+    Canvas &m_canvas;
+};
+
+TEST(RenderTest, RenderSequential)
+{
+    constexpr unsigned rows = 11;
+    constexpr unsigned cols = 11;
+    Canvas canvas{cols, rows};
+    CanvasWriter writer{canvas};
+
+    auto world = DefaultWorld();
+    auto from = Point(0, 0, -5);
+    auto to = Point(0, 0, 0);
+    auto up = Vector(0, 1, 0);
+    auto transform = ViewTransform(from, to, up);
+    Camera camera{11, 11, HalfPI, transform};
+
+    RenderSequentially(camera, world, writer);
+
+    auto color = canvas.GetPixel(5, 5);
+    EXPECT_EQ(Floats(Color(0.38066f, 0.47583f, 0.2855f)), Floats(color));
 }
 
 } // namespace zrt
