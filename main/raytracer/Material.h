2b857cafbd618d78a65932d5f9b82d482101b79c
Dirk Zimmermann <me@dirkz.com>
2025-02-24 12:10:30 +0100

    Infrastructure for patterns in the material

--- main/raytracer/Material.h
+++ main/raytracer/Material.h
@@ -6,13 +6,69 @@
 namespace zrt
 {
 
-struct Material
+struct ConstPattern
 {
-    Material(FXMVECTOR color = zrt::Color(1, 1, 1), float ambient = 0.1, float diffuse = 0.9,
-             float specular = 0.9, float shininess = 200);
+    inline XMVECTOR XM_CALLCONV operator()(FXMVECTOR c)
+    {
+        return c;
+    }
+};
 
-    XMVECTOR XM_CALLCONV Lighting(const PointLight &light, FXMVECTOR position, FXMVECTOR eyev,
-                                  FXMVECTOR normal, bool isInLight = true) const;
+template <class T> struct MaterialWithPattern
+{
+    MaterialWithPattern(FXMVECTOR color = zrt::Color(1, 1, 1), float ambient = 0.1,
+                        float diffuse = 0.9, float specular = 0.9, float shininess = 200,
+                        T pattern = ConstPattern{})
+        : m_ambient{ambient}, m_diffuse{diffuse}, m_specular{specular}, m_shininess{shininess},
+          m_pattern{pattern}
+    {
+        XMStoreFloat4(&m_color, color);
+    }
+
+    inline XMVECTOR XM_CALLCONV Lighting(const PointLight &light, FXMVECTOR position,
+                                         FXMVECTOR eyev, FXMVECTOR normal,
+                                         bool isInLight = true) const
+    {
+        XMVECTOR diffuse = XMVectorZero();
+        XMVECTOR specular = XMVectorZero();
+
+        XMVECTOR effectiveColor = XMColorModulate(Color(), light.Intensity());
+        XMVECTOR lightv = XMVector4Normalize(XMVectorSubtract(light.Position(), position));
+        XMVECTOR ambient = XMVectorScale(effectiveColor, Ambient());
+
+        float lightDotNormal = XMVectorGetX(XMVector4Dot(lightv, normal));
+        if (lightDotNormal < 0)
+        {
+            diffuse = zrt::Color(0, 0, 0);
+        }
+        else
+        {
+            diffuse = XMVectorScale(XMVectorScale(effectiveColor, Diffuse()), lightDotNormal);
+            XMVECTOR reflectv = Reflect(XMVectorScale(lightv, -1), normal);
+            float reflectDotEye = XMVectorGetX(XMVector4Dot(reflectv, eyev));
+            if (reflectDotEye <= 0)
+            {
+                specular = zrt::Color(0, 0, 0);
+            }
+            else
+            {
+                float factor = std::pow(reflectDotEye, Shininess());
+                specular = XMVectorScale(XMVectorScale(light.Intensity(), Specular()), factor);
+            }
+        }
+
+        XMVECTOR sumv = ambient;
+
+        if (isInLight)
+        {
+            sumv = XMVectorAdd(sumv, diffuse);
+            sumv = XMVectorAdd(sumv, specular);
+        }
+
+        sumv = XMVectorSetW(sumv, 1);
+
+        return sumv;
+    }
 
     inline XMVECTOR XM_CALLCONV Color() const
     {
@@ -50,6 +106,9 @@ struct Material
     float m_diffuse;
     float m_specular;
     float m_shininess;
+    T m_pattern;
 };
+
+using Material = MaterialWithPattern<ConstPattern>;
 
 } // namespace zrt
