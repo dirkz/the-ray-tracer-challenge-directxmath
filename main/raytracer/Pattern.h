2be926ddb63d513187b10a223c47df9b6f717988
Dirk Zimmermann <me@dirkz.com>
2025-02-24 18:59:01 +0100

    Delete the pattern that I don't want to maintain

--- main/raytracer/Pattern.h
+++ main/raytracer/Pattern.h
@@ -77,36 +77,4 @@ struct StripePattern : public Pattern
     XMFLOAT4 m_color;
 };
 
-struct TwoStripePattern : Pattern
-{
-    TwoStripePattern(FXMVECTOR color1, FXMVECTOR color2, FXMMATRIX transform = XMMatrixIdentity())
-        : Pattern{transform}
-    {
-        XMStoreFloat4(&m_color1, color1);
-        XMStoreFloat4(&m_color2, color2);
-    }
-
-    XMVECTOR XM_CALLCONV operator()(const Intersectable *object, FXMVECTOR position,
-                                    FXMVECTOR color) const
-    {
-        XMVECTOR patternPosition = PatternPosition(object, position);
-
-        float x = XMVectorGetX(patternPosition);
-        float m = std::fmod(std::abs(x), 2.f);
-        float fm = std::floor(m);
-        if (fm == 1.f)
-        {
-            return XMLoadFloat4(&m_color1);
-        }
-        else
-        {
-            return XMLoadFloat4(&m_color2);
-        }
-    }
-
-  private:
-    XMFLOAT4 m_color1;
-    XMFLOAT4 m_color2;
-};
-
 } // namespace zrt
