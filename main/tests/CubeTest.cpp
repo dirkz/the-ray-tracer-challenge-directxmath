9846d15581beff3ca03a82535a7144a36782ba08
Dirk Zimmermann <me@dirkz.com>
2025-02-28 10:43:15 +0100

    Better test naming

--- main/tests/CubeTest.cpp
+++ main/tests/CubeTest.cpp
@@ -8,10 +8,9 @@
 namespace zrt
 {
 
-struct IntersectionDataHit
+struct RayHitData
 {
-    IntersectionDataHit(FXMVECTOR origin, FXMVECTOR direction, float t1, float t2)
-        : m_t1{t1}, m_t2{t2}
+    RayHitData(FXMVECTOR origin, FXMVECTOR direction, float t1, float t2) : m_t1{t1}, m_t2{t2}
     {
         XMStoreFloat4(&m_origin, origin);
         XMStoreFloat4(&m_direction, direction);
@@ -44,9 +43,9 @@ struct IntersectionDataHit
     float m_t2;
 };
 
-struct IntersectionDataMiss
+struct RayMissData
 {
-    IntersectionDataMiss(FXMVECTOR origin, FXMVECTOR direction)
+    RayMissData(FXMVECTOR origin, FXMVECTOR direction)
     {
         XMStoreFloat4(&m_origin, origin);
         XMStoreFloat4(&m_direction, direction);
@@ -67,17 +66,17 @@ struct IntersectionDataMiss
     XMFLOAT4 m_direction;
 };
 
-struct IntersectionTestHit : public testing::TestWithParam<IntersectionDataHit>
+struct RayHit : public testing::TestWithParam<RayHitData>
 {
 };
 
-struct IntersectionTestMiss : public testing::TestWithParam<IntersectionDataMiss>
+struct RayMiss : public testing::TestWithParam<RayMissData>
 {
 };
 
-TEST_P(IntersectionTestHit, CubeIntersection)
+TEST_P(RayHit, RayHits)
 {
-    IntersectionDataHit param = GetParam();
+    RayHitData param = GetParam();
 
     Cube c{};
     Ray r{param.Origin(), param.Direction()};
@@ -89,9 +88,9 @@ TEST_P(IntersectionTestHit, CubeIntersection)
     EXPECT_FLOAT_EQ(xs[1].T(), param.T2());
 }
 
-TEST_P(IntersectionTestMiss, RayMisses)
+TEST_P(RayMiss, RayMisses)
 {
-    IntersectionDataMiss param = GetParam();
+    RayMissData param = GetParam();
 
     Cube c{};
     Ray r{param.Origin(), param.Direction()};
@@ -101,23 +100,22 @@ TEST_P(IntersectionTestMiss, RayMisses)
     EXPECT_EQ(xs.size(), 0);
 }
 
-INSTANTIATE_TEST_CASE_P(
-    CubeTest, IntersectionTestHit,
-    testing::Values(IntersectionDataHit{Point(5, 0.5f, 0), Vector(-1, 0, 0), 4, 6},
-                    IntersectionDataHit{Point(-5, 0.5f, 0), Vector(1, 0, 0), 4, 6},
-                    IntersectionDataHit{Point(0.5f, 5, 0), Vector(0, -1, 0), 4, 6},
-                    IntersectionDataHit{Point(0.5, -5, 0), Vector(0, 1, 0), 4, 6},
-                    IntersectionDataHit{Point(0.5f, 0, 5), Vector(0, 0, -1), 4, 6},
-                    IntersectionDataHit{Point(0.5, 0, -5), Vector(0, 0, 1), 4, 6},
-                    IntersectionDataHit{Point(0, 0.5, 0), Vector(0, 0, 1), -1, 1}));
+INSTANTIATE_TEST_CASE_P(CubeTest, RayHit,
+                        testing::Values(RayHitData{Point(5, 0.5f, 0), Vector(-1, 0, 0), 4, 6},
+                                        RayHitData{Point(-5, 0.5f, 0), Vector(1, 0, 0), 4, 6},
+                                        RayHitData{Point(0.5f, 5, 0), Vector(0, -1, 0), 4, 6},
+                                        RayHitData{Point(0.5, -5, 0), Vector(0, 1, 0), 4, 6},
+                                        RayHitData{Point(0.5f, 0, 5), Vector(0, 0, -1), 4, 6},
+                                        RayHitData{Point(0.5, 0, -5), Vector(0, 0, 1), 4, 6},
+                                        RayHitData{Point(0, 0.5, 0), Vector(0, 0, 1), -1, 1}));
 
 INSTANTIATE_TEST_CASE_P(
-    CubeTest, IntersectionTestMiss,
-    testing::Values(IntersectionDataMiss{Point(-2, 0, 0), Vector(0.2673f, 0.5345f, 0.8018f)},
-                    IntersectionDataMiss{Point(0, -2, 0), Vector(0.8018f, 0.2673f, 0.5345f)},
-                    IntersectionDataMiss{Point(0, 0, -2), Vector(0.5345f, 0.8018f, 0.2673f)},
-                    IntersectionDataMiss{Point(2, 0, 2), Vector(0, 0, -1)},
-                    IntersectionDataMiss{Point(0, 2, 2), Vector(0, -1, 0)},
-                    IntersectionDataMiss{Point(2, 2, 0), Vector(-1, 0, 0)}));
+    CubeTest, RayMiss,
+    testing::Values(RayMissData{Point(-2, 0, 0), Vector(0.2673f, 0.5345f, 0.8018f)},
+                    RayMissData{Point(0, -2, 0), Vector(0.8018f, 0.2673f, 0.5345f)},
+                    RayMissData{Point(0, 0, -2), Vector(0.5345f, 0.8018f, 0.2673f)},
+                    RayMissData{Point(2, 0, 2), Vector(0, 0, -1)},
+                    RayMissData{Point(0, 2, 2), Vector(0, -1, 0)},
+                    RayMissData{Point(2, 2, 0), Vector(-1, 0, 0)}));
 
 } // namespace zrt
