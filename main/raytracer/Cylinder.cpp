8e22f64a3919ed250f7266f711fe2c7a77afdc8b
Dirk Zimmermann <me@dirkz.com>
2025-02-28 17:44:33 +0100

    Implement parts of Cylinder::LocalIntersect

--- main/raytracer/Cylinder.cpp
+++ main/raytracer/Cylinder.cpp
@@ -1,11 +1,37 @@
 #include "Cylinder.h"
 
+#include "Constants.h"
+
 namespace zrt
 {
 
 std::vector<Intersection> Cylinder::LocalIntersect(const Ray &ray) const
 {
-    return {};
+    XMFLOAT4 origin;
+    XMStoreFloat4(&origin, ray.Origin());
+
+    XMFLOAT4 direction;
+    XMStoreFloat4(&direction, ray.Direction());
+
+    float a = direction.x * direction.x + direction.z * direction.z;
+
+    if (std::abs(a) < Epsilon)
+    {
+        return {};
+    }
+
+    float b = 2.f * origin.x * direction.x + 2.f * origin.z * direction.z;
+    float c = origin.x * origin.x + origin.z * origin.z - 1.f;
+
+    float disc = b * b - 4.f * a * c;
+
+    if (disc < 0)
+    {
+        return {};
+    }
+
+    // dummy
+    return {Intersection{this, 1.f}};
 }
 
 XMVECTOR XM_CALLCONV Cylinder::Normal(FXMVECTOR p) const
