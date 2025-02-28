5f698401771bfefb199dd4b384262e80de7bf50b
Dirk Zimmermann <me@dirkz.com>
2025-02-28 17:15:12 +0100

    Prefix all cube test data and fixtures

--- main/tests/CubeTest.cpp
+++ main/tests/CubeTest.cpp
@@ -9,9 +9,9 @@
 namespace zrt
 {
 
-struct RayHitData
+struct CubeRayHitData
 {
-    RayHitData(FXMVECTOR origin, FXMVECTOR direction, float t1, float t2) : m_t1{t1}, m_t2{t2}
+    CubeRayHitData(FXMVECTOR origin, FXMVECTOR direction, float t1, float t2) : m_t1{t1}, m_t2{t2}
     {
         XMStoreFloat4(&m_origin, origin);
         XMStoreFloat4(&m_direction, direction);
@@ -44,9 +44,9 @@ struct RayHitData
     float m_t2;
 };
 
-struct RayMissData
+struct CubeRayMissData
 {
-    RayMissData(FXMVECTOR origin, FXMVECTOR direction)
+    CubeRayMissData(FXMVECTOR origin, FXMVECTOR direction)
     {
         XMStoreFloat4(&m_origin, origin);
         XMStoreFloat4(&m_direction, direction);
@@ -67,9 +67,9 @@ struct RayMissData
     XMFLOAT4 m_direction;
 };
 
-struct NormalData
+struct CubeNormalData
 {
-    NormalData(FXMVECTOR point, FXMVECTOR normal)
+    CubeNormalData(FXMVECTOR point, FXMVECTOR normal)
     {
         XMStoreFloat4(&m_point, point);
         XMStoreFloat4(&m_normal, normal);
@@ -90,21 +90,21 @@ struct NormalData
     XMFLOAT4 m_normal;
 };
 
-struct RayHit : public testing::TestWithParam<RayHitData>
+struct CubeRayHit : public testing::TestWithParam<CubeRayHitData>
 {
 };
 
-struct RayMiss : public testing::TestWithParam<RayMissData>
+struct CubeRayMiss : public testing::TestWithParam<CubeRayMissData>
 {
 };
 
-struct Normal : public testing::TestWithParam<NormalData>
+struct CubeNormal : public testing::TestWithParam<CubeNormalData>
 {
 };
 
-TEST_P(RayHit, RayHits)
+TEST_P(CubeRayHit, RayHits)
 {
-    RayHitData param = GetParam();
+    CubeRayHitData param = GetParam();
 
     Cube c{};
     Ray r{param.Origin(), param.Direction()};
@@ -116,9 +116,9 @@ TEST_P(RayHit, RayHits)
     EXPECT_FLOAT_EQ(xs[1].T(), param.T2());
 }
 
-TEST_P(RayMiss, RayMisses)
+TEST_P(CubeRayMiss, RayMisses)
 {
-    RayMissData param = GetParam();
+    CubeRayMissData param = GetParam();
 
     Cube c{};
     Ray r{param.Origin(), param.Direction()};
@@ -128,9 +128,9 @@ TEST_P(RayMiss, RayMisses)
     EXPECT_EQ(xs.size(), 0);
 }
 
-TEST_P(Normal, Normals)
+TEST_P(CubeNormal, Normals)
 {
-    NormalData param = GetParam();
+    CubeNormalData param = GetParam();
 
     Cube c{};
 
@@ -141,32 +141,32 @@ TEST_P(Normal, Normals)
     EXPECT_EQ(Floats(n), Floats(normal));
 }
 
-INSTANTIATE_TEST_CASE_P(CubeTest, RayHit,
-                        testing::Values(RayHitData{Point(5, 0.5f, 0), Vector(-1, 0, 0), 4, 6},
-                                        RayHitData{Point(-5, 0.5f, 0), Vector(1, 0, 0), 4, 6},
-                                        RayHitData{Point(0.5f, 5, 0), Vector(0, -1, 0), 4, 6},
-                                        RayHitData{Point(0.5, -5, 0), Vector(0, 1, 0), 4, 6},
-                                        RayHitData{Point(0.5f, 0, 5), Vector(0, 0, -1), 4, 6},
-                                        RayHitData{Point(0.5, 0, -5), Vector(0, 0, 1), 4, 6},
-                                        RayHitData{Point(0, 0.5, 0), Vector(0, 0, 1), -1, 1}));
+INSTANTIATE_TEST_CASE_P(CubeTest, CubeRayHit,
+                        testing::Values(CubeRayHitData{Point(5, 0.5f, 0), Vector(-1, 0, 0), 4, 6},
+                                        CubeRayHitData{Point(-5, 0.5f, 0), Vector(1, 0, 0), 4, 6},
+                                        CubeRayHitData{Point(0.5f, 5, 0), Vector(0, -1, 0), 4, 6},
+                                        CubeRayHitData{Point(0.5, -5, 0), Vector(0, 1, 0), 4, 6},
+                                        CubeRayHitData{Point(0.5f, 0, 5), Vector(0, 0, -1), 4, 6},
+                                        CubeRayHitData{Point(0.5, 0, -5), Vector(0, 0, 1), 4, 6},
+                                        CubeRayHitData{Point(0, 0.5, 0), Vector(0, 0, 1), -1, 1}));
 
 INSTANTIATE_TEST_CASE_P(
-    CubeTest, RayMiss,
-    testing::Values(RayMissData{Point(-2, 0, 0), Vector(0.2673f, 0.5345f, 0.8018f)},
-                    RayMissData{Point(0, -2, 0), Vector(0.8018f, 0.2673f, 0.5345f)},
-                    RayMissData{Point(0, 0, -2), Vector(0.5345f, 0.8018f, 0.2673f)},
-                    RayMissData{Point(2, 0, 2), Vector(0, 0, -1)},
-                    RayMissData{Point(0, 2, 2), Vector(0, -1, 0)},
-                    RayMissData{Point(2, 2, 0), Vector(-1, 0, 0)}));
+    CubeTest, CubeRayMiss,
+    testing::Values(CubeRayMissData{Point(-2, 0, 0), Vector(0.2673f, 0.5345f, 0.8018f)},
+                    CubeRayMissData{Point(0, -2, 0), Vector(0.8018f, 0.2673f, 0.5345f)},
+                    CubeRayMissData{Point(0, 0, -2), Vector(0.5345f, 0.8018f, 0.2673f)},
+                    CubeRayMissData{Point(2, 0, 2), Vector(0, 0, -1)},
+                    CubeRayMissData{Point(0, 2, 2), Vector(0, -1, 0)},
+                    CubeRayMissData{Point(2, 2, 0), Vector(-1, 0, 0)}));
 
-INSTANTIATE_TEST_CASE_P(CubeTest, Normal,
-                        testing::Values(NormalData{Point(1, 0.5f, -0.8f), Vector(1, 0, 0)},
-                                        NormalData{Point(-1, -0.2f, 0.9f), Vector(-1, 0, 0)},
-                                        NormalData{Point(-0.4f, 1, -0.1f), Vector(0, 1, 0)},
-                                        NormalData{Point(0.3f, -1, -0.7f), Vector(0, -1, 0)},
-                                        NormalData{Point(-0.6f, 0.3f, 1), Vector(0, 0, 1)},
-                                        NormalData{Point(0.4f, 0.4f, -1), Vector(0, 0, -1)},
-                                        NormalData{Point(1, 1, 1), Vector(1, 0, 0)},
-                                        NormalData{Point(-1, -1, -1), Vector(-1, 0, 0)}));
+INSTANTIATE_TEST_CASE_P(CubeTest, CubeNormal,
+                        testing::Values(CubeNormalData{Point(1, 0.5f, -0.8f), Vector(1, 0, 0)},
+                                        CubeNormalData{Point(-1, -0.2f, 0.9f), Vector(-1, 0, 0)},
+                                        CubeNormalData{Point(-0.4f, 1, -0.1f), Vector(0, 1, 0)},
+                                        CubeNormalData{Point(0.3f, -1, -0.7f), Vector(0, -1, 0)},
+                                        CubeNormalData{Point(-0.6f, 0.3f, 1), Vector(0, 0, 1)},
+                                        CubeNormalData{Point(0.4f, 0.4f, -1), Vector(0, 0, -1)},
+                                        CubeNormalData{Point(1, 1, 1), Vector(1, 0, 0)},
+                                        CubeNormalData{Point(-1, -1, -1), Vector(-1, 0, 0)}));
 
 } // namespace zrt
