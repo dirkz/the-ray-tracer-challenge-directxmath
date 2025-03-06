1ee8290bd0697c805f34ea21df2062c1235c6004
Dirk Zimmermann <me@dirkz.com>
2025-03-06 20:34:04 +0100

    Test for world to object space

--- main/tests/ShapeTest.cpp
+++ main/tests/ShapeTest.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
 
+#include "TestConstants.h"
+#include "TestUtil.h"
+
+#include "Group.h"
+#include "Matrix.h"
 #include "Shape.h"
 #include "Sphere.h"
 
@@ -10,6 +15,17 @@ TEST(ShapeTest, ShapeHasParentAttribute)
 {
     Sphere s{};
     EXPECT_EQ(s.Parent(), nullptr);
+}
+
+TEST(ShapeTest, ConvertingPointFromWorldToObjectSpace)
+{
+    Group g1{RotationY(HalfPI)};
+    Group g2{Scaling(2, 2, 2)};
+    g1.Add(&g2);
+    Sphere s{Translation(5, 0, 0)};
+    g2.Add(&s);
+    auto p = s.WorldToObjectPoint(Point(-2, 0, -10));
+    EXPECT_EQ(Floats(p), Floats(0, 0, -1));
 }
 
 } // namespace zrt
